42f3e2be9ecdcc77c36bed115d45fcc27ed08c51
Nicolas Brunie <nibrunie@gmail.com>
2019-11-04 13:57:07 +0100

    Refactoring random generation and adding test generation for fp64_add_ro

--- cr_fp_dot_emulation.py
+++ cr_fp_dot_emulation.py
@@ -71,14 +71,22 @@ def exact_dp4(a_v, b_v):
         acc = p + acc
     return acc
 
-def random_fp32_value():
+def random_fp32_value(emin=-126, emax=127):
+    return random_fp_value(emin=emin, emax=emax, precision=sollya.binary32)
+def random_fp64_value(emin=-1022, emax=1023):
+    return random_fp_value(emin=emin, emax=emax, precision=sollya.binary64)
+
+def random_fp_value(emin=-126, emax=127, precision=sollya.binary32):
     """ Dummy generation for random binary32/fp32 value
 
         :return: random fp32 number
         :rtype: SollyaObject
     """
-    real_value = random.randrange(-1, 2, 2) * S2**random.randrange(-126, 127) * random.random()
-    return sollya.round(real_value, sollya.binary32, sollya.RN)
+    # FIXME: almost real, python's random.random() returns a number in [0, 1]
+    # (possibly internally mapped) to a double, which is not really a good
+    # way to generate a proper mantissa
+    real_value = random.randrange(-1, 2, 2) * S2**random.randrange(emin, emax+1) * (1.0 + random.random())
+    return sollya.round(real_value, precision, sollya.RN)
 
 def random_4d_vector():
     """ Random 4D vector
@@ -89,14 +97,37 @@ def random_4d_vector():
     vector = [random_fp32_value() for i in range(4)]
     return vector
 
-TEST_NUM = 10000000
 
-for i in range(TEST_NUM):
-    a_v, b_v = random_4d_vector(), random_4d_vector()
-    exact_result = exact_dp4(a_v, b_v)
-    cr_result = sollya.round(exact_result, sollya.binary32, sollya.RN)
-    new_method_result = dp4_cr(a_v, b_v)
-    if cr_result != new_method_result:
-        print("{} vs {} mismatch".format(cr_result, new_method_result))
-        sys.exit(1)
 
+def generate_round_to_odd_test(test_num):
+    print("unsigned test_num = {};".format(test_num))
+    print("double fp64_add_ro_test[{}][3] = {{".format(test_num * 2))
+    # random
+    for i in range(test_num):
+        lhs = random_fp64_value()
+        rhs = random_fp64_value()
+        expected = round_to_odd(lhs + rhs, sollya.binary64)
+        print("    {{{}, {}, {}}},".format(lhs, rhs, expected))
+    # close test
+    for i in range(test_num):
+        lhs = random_fp64_value(emin=-2, emax=2)
+        rhs = random_fp64_value(emin=-2, emax=2)
+        expected = round_to_odd(lhs + rhs, sollya.binary64)
+        print("    {{{}, {}, {}}},".format(lhs, rhs, expected))
+    print("};")
+
+if __name__ == "__main__":
+    # testing dp4_cr
+    TEST_NUM = 100
+
+    for i in range(TEST_NUM):
+        a_v, b_v = random_4d_vector(), random_4d_vector()
+        exact_result = exact_dp4(a_v, b_v)
+        cr_result = sollya.round(exact_result, sollya.binary32, sollya.RN)
+        new_method_result = dp4_cr(a_v, b_v)
+        if cr_result != new_method_result:
+            print("{} vs {} mismatch".format(cr_result, new_method_result))
+            sys.exit(1)
+
+    # generating test for fp64_add_ro
+    generate_round_to_odd_test(10)
